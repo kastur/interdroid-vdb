bcb3365b14ea0686c19b5c9b415f05fc8eca735b
Nick Palmer <palmer@cs.vu.nl>
2010-07-15 15:36:23 +0000

    Split ORM and Avro versions of Notepad out of the repository.

--- src/com/google/provider/versioned/orm/NotePadProvider.java
+++
@@ -1,9 +0,0 @@
-package com.google.provider.versioned.orm;
-
-import interdroid.vdb.content.orm.ORMGenericContentProvider;
-
-public class NotePadProvider extends ORMGenericContentProvider {
-	public NotePadProvider() {
-		super("notes", Notes.class);
-	}
-}
