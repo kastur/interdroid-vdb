29b06ffda041ca57507492963855b381a1daf7e0
nickpalmer <nick@sluggardy.net>
2011-07-13 16:17:57 +0200

    Clean up Actions based on checkstyle output.

--- src/interdroid/vdb/Actions.java
+++ src/interdroid/vdb/Actions.java
@@ -1,29 +1,65 @@
+package interdroid.vdb;
+
 /**
  * This class holds constants for triggering actions in the vdb-ui package.
  * It has to live here due to the way the android tools structure Library
  * packages. We can't make vdb-ui a Library to import this class into
  * other UI packages that want to launch ui activities so it lives down
  * in this layer even though it really belongs in the vdb-ui system.
- */
-package interdroid.vdb;
-
-/**
  *
  * @author nick
  *
  */
 public final class Actions {
-	private static final String ACTION_BASE = "interdroid.vdb.action.";
-	public static final String ACTION_COMMIT = ACTION_BASE + "COMMIT";
-	public static final String ACTION_ADD_BRANCH = ACTION_BASE + "ADD_BRANCH";
-	public static final String ACTION_EDIT_REMOTE = ACTION_BASE + "EDIT_REMOTE";
-	public static final String ACTION_ADD_REMOTE = ACTION_BASE + "ADD_REMOTE";
-	public static String ACTION_MANAGE_LOCAL_BRANCHES = ACTION_BASE + "MANAGE_LOCAL_BRANCHES";
-	public static final String ACTION_MANAGE_REMOTES = ACTION_BASE + "MANAGE_REMOTES";
-	public static final String ACTION_ADD_PEER = ACTION_BASE + "ADD_PEER";
-	public static final String ACTION_MANAGE_PEERS = ACTION_BASE + "MANAGE_PEERS";
-	public static final String ACTION_MANAGE_REPOSITORY_PROPERTIES = ACTION_BASE + "MANAGE_REPOSITORY_PROPERTIES";
-	public static String ACTION_MANAGE_REPOSITORY = ACTION_BASE + "MANAGE_REPOSITORY";
-	public static String ACTION_MANAGE_REPOSITORIES = ACTION_BASE + "MANAGE_REPOSITORIES";
+    /** Don't allow construction of this class. **/
+    private Actions() { }
+
+    /** Base for all actions. **/
+    private static final String ACTION_BASE =
+            "interdroid.vdb.action.";
+
+    /** The Commit activity action. **/
+    public static final String ACTION_COMMIT =
+            ACTION_BASE + "COMMIT";
+
+    /** Add a branch action. **/
+    public static final String ACTION_ADD_BRANCH =
+            ACTION_BASE + "ADD_BRANCH";
+
+    /** Edit a remote hub action. **/
+    public static final String ACTION_EDIT_REMOTE =
+            ACTION_BASE + "EDIT_REMOTE";
+
+    /** Add a peer action. **/
+    public static final String ACTION_ADD_REMOTE =
+            ACTION_BASE + "ADD_REMOTE";
+
+    /** Manage local branches action. **/
+    public static final String ACTION_MANAGE_LOCAL_BRANCHES =
+            ACTION_BASE + "MANAGE_LOCAL_BRANCHES";
+
+    /** Manage all remote repositories action. **/
+    public static final String ACTION_MANAGE_REMOTES =
+            ACTION_BASE + "MANAGE_REMOTES";
+
+    /** Add a peer action. **/
+    public static final String ACTION_ADD_PEER =
+            ACTION_BASE + "ADD_PEER";
+
+    /** Manage all peers action. **/
+    public static final String ACTION_MANAGE_PEERS =
+            ACTION_BASE + "MANAGE_PEERS";
+
+    /** Manage the properties for a repository. **/
+    public static final String ACTION_MANAGE_REPOSITORY_PROPERTIES =
+            ACTION_BASE + "MANAGE_REPOSITORY_PROPERTIES";
+
+    /** Manage a particular repository action. **/
+    public static final String ACTION_MANAGE_REPOSITORY =
+            ACTION_BASE + "MANAGE_REPOSITORY";
+
+    /** Manage all repositories action. **/
+    public static final String ACTION_MANAGE_REPOSITORIES =
+            ACTION_BASE + "MANAGE_REPOSITORIES";
 
 }
---
+++ src/interdroid/vdb/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The base package for the vdb project.
+ **/
+package interdroid.vdb;
